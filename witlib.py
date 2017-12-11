640db8649e25e6e549ed1cdfd28e29d147b8c12c
ayusheesingh <ayushee.singh@gmail.com>
2017-12-10 23:37:58 -0600

    Changed tabs to spaces.

--- witlib.py
+++ witlib.py
@@ -5,17 +5,17 @@ import sys
 from wit import Wit # Refer: https://github.com/wit-ai/pywit
 
 class WitAdapter:
-	def __init__(self, token):
-		"""Create object with wit access token"""
-		self.wit = Wit(token)
+    def __init__(self, token):
+        """Create object with wit access token"""
+        self.wit = Wit(token)
 
-	def response(self, message):
-		"""Returns wit response"""
+    def response(self, message):
+        """Returns wit response"""
 
-		try:
-			result = self.wit.message(message)
-			print("Got response from wit.ai", json.dumps(result, indent=2))
+        try:
+            result = self.wit.message(message)
+            print("Got response from wit.ai", json.dumps(result, indent=2))
             sys.stdout.flush()
-			return list(result["entities"]["intent"])[0]["value"]
-		except:
-			return "Sorry, I didn't understand!"
+            return list(result["entities"]["intent"])[0]["value"]
+        except:
+            return "Sorry, I didn't understand!"
